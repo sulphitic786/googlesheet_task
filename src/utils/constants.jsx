170927773fc073ef89b1851c7495a92cf4daf51e
qamarkhanInnovo <wqasimg@gmail.com>
2024-09-18 01:12:24 +0500

    Final update is done with constant

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,2 @@
 export const pdfUrl = "https://drive.google.com/file/d/1cW9xxZucYY2pAGntr5oDI0SKzWjIG3Sw/view?usp=drive_link"
-export const fileURL ="https://sheet.best/api/sheets/1fa9a6b6-db87-4a0a-a166-23d693217f4f";
+export const fileURL ="https://sheet.best/api/sheets/1fa9a6b6-db87-4a0a-a166-23d693217f4f"; //changed
